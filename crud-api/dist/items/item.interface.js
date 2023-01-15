b82462b2935ceaa076247706181a6f690c55ad90
sesychev <sergeysychou@gmail.com>
2023-01-15 22:29:03 +0300

    feat: Untrack files in .gitignore

--- crud-api/dist/items/item.interface.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-// src/items/item.interface.ts
-Object.defineProperty(exports, "__esModule", { value: true });
-//https://stackoverflow.com/questions/43837659/guid-uuid-in-typescript-node-js-app
-//# sourceMappingURL=item.interface.js.map
